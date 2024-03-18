5bd63d3251cc6462644266ae2bde5175fc92e13f
Lorenzo Piarulli <lorenzo.piarulli3000@gmail.com>
2024-03-18 13:57:36 +0100

    some on ex1

--- Ex1_MonteCarloSampling/MCSampling.cpp
+++ Ex1_MonteCarloSampling/MCSampling.cpp
@@ -1,4 +1,5 @@
 #include <algorithm>
+#include <cmath>
 #include <mpi.h>
 #include <random>
 #include <iostream>
@@ -21,9 +22,6 @@ int main(int argc, char *argv[]){
     int id;
     MPI_Comm_rank(MPI_COMM_WORLD, &id);
 
-    int batchSize = N/size;
-
-
     int squarel = 2; //so the range is between 0<=x<=2 and 0<=y<=2
     int circler = 1; //so the range is ?
 
@@ -33,34 +31,56 @@ int main(int argc, char *argv[]){
         set<float> unique_numbers; //No repetitions
         
         int n = N;
-        MPI_Bcast(&n, 1, MPI_INT, 0, MPI_COMM_WORLD);
+        int batchSize = (N)/size;
+        MPI_Bcast(&batchSize, 1, MPI_INT, 0, MPI_COMM_WORLD);
 
         float value;
         while(unique_numbers.size() < N){
-            MPI_Status status;
-            MPI_Recv(&value, 1, MPI_FLOAT, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, &status);
+            MPI_Status tmpStatus;
+            
+            cout << "start " << (N)-unique_numbers.size() << endl;
+
+            MPI_Recv(&value, 1, MPI_FLOAT, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, &tmpStatus);
+
+            cout << "end" << endl; 
 
             unique_numbers.insert(value);
 
             if(value<0){
-                int remains = (N)-unique_numbers.size();
-                MPI_Send(&remains, 1, MPI_INT, status.MPI_SOURCE, 0, MPI_COMM_WORLD);
+                int remains;
+                if(size>((N)-unique_numbers.size())){
+                    remains = size;
+                } else {
+                    remains = ((N)-unique_numbers.size())/size;
+                }
+                MPI_Send(&remains, 1, MPI_INT, tmpStatus.MPI_SOURCE, 0, MPI_COMM_WORLD);
             }
+        }
+        cout << id << endl; 
+
+
+        for(int i = 0; i<size; i++){
+            MPI_Status tmpStatus;
+            float ret;
+            MPI_Recv(&ret, 1, MPI_FLOAT, i, 0, MPI_COMM_WORLD, &tmpStatus);
         }
 
         MPI_Barrier(MPI_COMM_WORLD);
-
-        cout << unique_numbers.size() << endl;
+        
+        
+        cout << "Result: " << unique_numbers.size() << endl;
+        /*
         for (set<float>::iterator i = unique_numbers.begin(); i != unique_numbers.end(); i++) {
             float element = *i;
             cout << element << ",";
         }
+        */
 
     } else {
 
         random_device rd;
         mt19937 gen(rd());
-        //vector<float>; 
+
         int bound;
 
         MPI_Bcast(&bound, 1, MPI_INT, 0, MPI_COMM_WORLD);
@@ -77,15 +97,17 @@ int main(int argc, char *argv[]){
             MPI_Status status;
             float askValue= -0.1;
 
-            cout << "REQUEST BOUND: " << bound << endl; 
+            cout << "REQUEST FROM: " << id << endl; 
 
             MPI_Send(&askValue, 1, MPI_FLOAT, 0, 0, MPI_COMM_WORLD);
             bound = 0;
             counter = 0;
             MPI_Recv(&bound, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
 
-            cout << "RECEIVED BOUND: " << bound << endl; 
+            cout << id << " RECEIVED BOUND: " << bound << endl; 
         }
+
+        cout << "I'm " << id << endl;
 
         MPI_Barrier(MPI_COMM_WORLD);
     }
