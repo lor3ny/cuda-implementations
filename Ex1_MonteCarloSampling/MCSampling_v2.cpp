c808570fd709374b3602b242332ffbb26fbad203
Lorenzo Piarulli <lorenzo.piarulli3000@gmail.com>
2024-03-19 18:46:28 +0100

    Abstraction of datatype and circle verification (repetitions checking still missing)

--- Ex1_MonteCarloSampling/MCSampling_v2.cpp
+++ Ex1_MonteCarloSampling/MCSampling_v2.cpp
@@ -6,12 +6,23 @@
 #include <iostream>
 #include <set>
 
-
-#define N 1<<10
+#define N 1<<15
 
 
 using namespace std;
 
+struct Coords {
+    int x;
+    int y;
+    char location;
+
+    bool operator<(const Coords& other) const {
+        if(x == other.x && y == other.y && location == other.location){
+            return true;
+        }
+        return false;
+    }
+};
 
 
 int main(int argc, char *argv[]){
@@ -26,12 +37,27 @@ int main(int argc, char *argv[]){
     int squarel = 2; //so the range is between 0<=x<=2 and 0<=y<=2
     int circler = 1; //so the range is ?
 
+    // DATATYPE CREATION
+
+    MPI_Datatype mpi_coords;
+
+    int blockLengths[3] = {1, 1, 1};
+    MPI_Aint displacements[3] = {0, sizeof(int), 2*sizeof(int)};
+
+    MPI_Datatype types[3] = {MPI_INT, MPI_INT, MPI_CHAR};
+
+    MPI_Type_create_struct(3, blockLengths, displacements, types, &mpi_coords);
+
+    MPI_Type_commit(&mpi_coords);
+    
 
     if(id==0){
         random_device rd;
         mt19937 gen(rd());
 
-        set<float> unique_numbers; //No repetitions
+        // Non stiamo facendo checking delle ripetizioni, e' necessario
+        vector<Coords> s_coords;
+        vector<Coords> c_coords; 
         
         int n = N;
         int batchSize = (N)/(size-1);
@@ -39,10 +65,10 @@ int main(int argc, char *argv[]){
         // Con Iprobe e' possibile rendere questo processo lavoratore
 
         MPI_Status status;
-        while(unique_numbers.size() < n){
+        while(s_coords.size()< n){
             int counter = 0;
 
-            float batchSize =  ((float)(N)-unique_numbers.size())/(float)(size);
+            float batchSize =  ((float)(N)-s_coords.size()/(float)(size));
             if(batchSize == 0){
                 break;
             }
@@ -54,28 +80,41 @@ int main(int argc, char *argv[]){
             int flag;
             MPI_Status probStatus;
             int batchCounter;
+
             do {
                 // HANDLE COMUNICATION
                 MPI_Iprobe(MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &flag, &probStatus);
 
                 if(flag){
-                    int ret;
-                    MPI_Recv(&ret, 1, MPI_FLOAT, probStatus.MPI_SOURCE, probStatus.MPI_TAG, MPI_COMM_WORLD, &status);
-                    if(ret == -1){
+                    Coords ret;
+                    MPI_Recv(&ret, 1, mpi_coords, probStatus.MPI_SOURCE, probStatus.MPI_TAG, MPI_COMM_WORLD, &status);
+                    if(ret.x == -1){
                         counter++;
-                    } else if(unique_numbers.size() < n) {
-                        unique_numbers.insert(ret);
+                    } else if(s_coords.size() < n) {
+                        s_coords.push_back(ret);
+                        if(ret.location == 'C')
+                            c_coords.push_back(ret);
                     }
                     flag = !flag;
                 }
                 // HANDLE COMUNICATION
 
                 // DO COMPUTATION
-                if(unique_numbers.size() == n){
+                if(s_coords.size() == n){
                     break;
                 }
-                float rand_n = ((float) uniform_int_distribution<int>(0,2000)(gen));
-                unique_numbers.insert(rand_n);
+                Coords rand_coord;
+                rand_coord.x = (uniform_int_distribution<int>(0,2000)(gen));
+                rand_coord.y = (uniform_int_distribution<int>(0,2000)(gen));
+
+                if(sqrt(pow((float)rand_coord.x, 2) + pow(rand_coord.y, 2)) < 1000){
+                    rand_coord.location = 'C';
+                    c_coords.push_back(rand_coord);
+                } else {
+                    rand_coord.location = 'S';
+                }
+                s_coords.push_back(rand_coord);
+                
                 batchCounter++;
 
                 if(batchCounter >= roundedSize){
@@ -91,12 +130,10 @@ int main(int argc, char *argv[]){
     
         cout << "I'm " << id << endl;
 
-        cout << unique_numbers.size() << endl;
-
         MPI_Barrier(MPI_COMM_WORLD);
 
-        // Create and MPI_Datatype to handle not int but tuple of int
-        // Sign which are inside the circle and which are outside, with the norm 
+        cout << "TOTAL POINTS: " << s_coords.size() << endl;
+        cout << "APPROXIMATED PI: " << (float)s_coords.size()/(float)c_coords.size() << endl;
         
     } else {
 
@@ -110,13 +147,20 @@ int main(int argc, char *argv[]){
         do{
             int counter = 0;
             while(counter < batchN){
-                float rand_n = ((float) uniform_int_distribution<int>(0,2000)(gen));
-                MPI_Send(&rand_n, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
+                Coords rand_coord;
+                rand_coord.x = (uniform_int_distribution<int>(0,2000)(gen));
+                rand_coord.y = (uniform_int_distribution<int>(0,2000)(gen));
+                if( sqrt(pow(rand_coord.x, 2) + pow(rand_coord.y, 2)) < 1000){
+                    rand_coord.location = 'C';
+                } else {
+                    rand_coord.location = 'S';
+                }
+                MPI_Send(&rand_coord, 1, mpi_coords, 0, 0, MPI_COMM_WORLD);
                 counter++;
             }
-
-            int ending = -1;
-            MPI_Send(&ending, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
+            Coords end_coord;
+            end_coord.x = -1;
+            MPI_Send(&end_coord, 1, mpi_coords, 0, 0, MPI_COMM_WORLD);
 
             MPI_Bcast(&batchN, 1, MPI_INT, 0, MPI_COMM_WORLD);
         } while (batchN > 0);
@@ -127,6 +171,8 @@ int main(int argc, char *argv[]){
     }
 
     // Printing;
+
+    MPI_Type_free(&mpi_coords);
 
     MPI_Finalize();
 
